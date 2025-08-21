b1a611f8a7601381e2fd4f1a7db6db0d8fd57559
Vickysonaiya <vickysonaiya0189@gmail.com>
2025-08-21 20:23:10 +0530

    error

--- src/PhoneNumberScreen.js
+++ src/PhoneNumberScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './PhoneNumberScreen.css';
+import './PhoneNumberScreen.css'
 
 function PhoneNumberScreen() {
   const [phone, setPhone] = useState('');
